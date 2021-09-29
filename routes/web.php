628614cb5c7b5170fd0c8d6501c4aae08f174623
Phelipe Stering <phelipe.stering@hotmail.com>
2021-09-28 21:48:06 -0300

    publicando fotos - com problemas - revisar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Su
+pport\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
