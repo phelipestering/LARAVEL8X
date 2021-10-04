dccf8fc52609fc94fc372f0f18562157da32d848
Phelipe Stering <phelipe.stering@hotmail.com>
2021-10-04 20:07:55 -0300

    criando grupo de rotas autenticadas

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index') ->middleware(['auth']);
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::POST('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::Delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/edit/{id}', [PostController::class, 'update'])->name('posts.update');
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index') ->middleware(['auth']);
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::POST('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::Delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/edit/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
